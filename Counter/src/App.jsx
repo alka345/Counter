2665de29485187bd0436a2ce124adfd2b46553a3
alka345 <alkaraojnv@gmail.com>
2024-01-27 17:48:54 +0530

    Code Review

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -12,14 +10,26 @@ function App() {
     setCount(count - 1)
   }
 
+ 
   return (
-    <>
-      <h1>Counter</h1>
+    <div className='w-full h-screen flex flex-wrap justify-center items-center py-5 '
+ 
+   >
+    <div className='max-w-md mx-auto  shadow-md rounded-lg px-4 py-5 my-7 mt-10 text-black font-medium  bg-slate-300'>
+
+      <h1 className='text-black text-center my-5 font-extrabold'>Counter</h1>
+      <button 
+        onClick={addValue}
+        className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'
+        >Add Value:{count <20 ? count : "20"}
+
+        </button>
       <button
-        onClick={addValue}>Add Value:{count <20 ? count : "20"}</button>
-      <button
-        onClick={removeValue}>Remove Value:{count > 0 ? count : "0"}</button>
-    </>
+        onClick={removeValue}
+        className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded ml-5'
+        >Remove Value:{count > 0 ? count : "0"}</button>
+    </div>
+    </div>
   )
 }
 
