17dc568b92187cf2230d823739a364504e2e6b91
alka345 <alkaraojnv@gmail.com>
2024-03-27 14:21:21 +0530

    feat

--- counterPractise/src/App.jsx
+++ counterPractise/src/App.jsx
@@ -6,20 +6,40 @@ import { useState } from 'react'
 function App() {
   let [count, setCount] = useState(0)
 
-  function addValue(){
-      setCount(count + 1);
-      console.log(count);
+  // function addValue(){
+  //   if (count < 20) {
+  //     setCount(count + 1);      
+  //   }
+
+  //     console.log(count);
+  // }
+  let addValue = () =>{
+    if (count < 20) {
+          setCount(count + 1);      
+        }
+    
+          console.log(count);
   }
-  function removeValue(){
-      setCount(count - 1);
-      console.log(count);
+  // function removeValue(){
+  //   if (count > 0) {
+  //     setCount(count - 1);      
+  //   }
+
+  //     console.log(count);
+  // }
+  let removeValue = () => {
+    if (count > 0) {
+          setCount(count - 1);      
+        }
+    
+          console.log(count); 
   }
 
   return (
     
  <div className='h-screen w-full bg-slate-300 flex items-center justify-center rounded'>
   <div className='bg-slate-800 text-white flex-col items-center justify-center p-11'> 
-  <h1 className='flex items-center justify-center mb-11 font-bold text-3xl'>counter</h1>
+  <h1 className='flex items-center justify-center mb-11 font-bold text-3xl'>Counter</h1>
   <p className='flex items-center justify-center bg-white p-5 mb-11 text-black'>{count}</p>
   <button 
   onClick={addValue}
